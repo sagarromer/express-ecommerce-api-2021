b1dd16baf35a238b0e9bb09b06dffa9270ca6b2b
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-09-27 08:50:58 -0700

    reading environment variables configured

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+require('dotenv/config');
+
+const api = process.env.API_URL;
 
 app.get('/', (req, res) => {
     res.send('hello api !')
