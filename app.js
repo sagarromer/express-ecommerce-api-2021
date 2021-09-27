b2a5906efa73df91104c11c5b750d135ba76c281
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-09-27 11:53:54 -0700

    mongoose connected to mongodb db

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 require('dotenv/config');
 //middleware
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/express_eshop', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+}).then(() => {console.log('mongo started');
+});
 const api = process.env.API_URL;
 
 app.get(`${api}/products`, (req, res) => {
