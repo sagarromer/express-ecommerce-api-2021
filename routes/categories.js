1131ff55a25661fee7c2f160afa637666327e032
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-12 22:21:37 -0700

    get categories and category details implemented

--- routes/categories.js
+++ routes/categories.js
@@ -10,7 +10,14 @@ router.get(`/`, async (req, res) =>{
     } 
     res.send(categoryList);
 })
+router.get('/:id', async(req,res)=>{
+    const category = await Category.findById(req.params.id);
 
+    if(!category) {
+        res.status(500).json({message: 'The category with the given ID was not found.'})
+    } 
+    res.status(200).send(category);
+})
 router.post('/', async (req,res)=>{
     let category = new Category({
         name: req.body.name,
