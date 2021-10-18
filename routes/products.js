b21a5394275a413633fb3d9ca0eaeab227ca4a7b
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-17 23:33:33 -0700

    delete a product rest api implemented

--- routes/products.js
+++ routes/products.js
@@ -79,4 +79,15 @@ router.put('/:id',async (req, res)=> {
 
     res.send(product);
 })
+router.delete('/:id', (req, res)=>{
+    Product.findByIdAndRemove(req.params.id).then(product =>{
+        if(product) {
+            return res.status(200).json({success: true, message: 'the product is deleted!'})
+        } else {
+            return res.status(404).json({success: false , message: "product not found!"})
+        }
+    }).catch(err=>{
+        return res.status(500).json({success: false, error: err}) 
+    })
+})
 module.exports =router; 
