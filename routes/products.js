ba48ee07841be58ae17e7904b07e52ce4336a7a8
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-17 23:30:11 -0700

    update a product rest api implemented

--- routes/products.js
+++ routes/products.js
@@ -49,4 +49,34 @@ router.post(`/`, async (req, res) =>{
 
     res.send(product);
 })
+router.put('/:id',async (req, res)=> {
+    if(!mongoose.isValidObjectId(req.params.id)) {
+        return res.status(400).send('Invalid Product Id')
+    }
+    const category = await Category.findById(req.body.category);
+    if(!category) return res.status(400).send('Invalid Category')
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription,
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        { new: true}
+    )
+
+    if(!product)
+    return res.status(500).send('the product cannot be updated!')
+
+    res.send(product);
+})
 module.exports =router; 
