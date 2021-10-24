520e1e8fbcb3daefee0bb0088ff1664ebdc5b8f8
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-23 17:05:21 -0700

    post register new user implemented

--- app.js
+++ app.js
@@ -13,16 +13,16 @@ app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
 //Routes
-//const categoriesRoutes = require('./routes/categories');
+const categoriesRoutes = require('./routes/categories');
 const productsRoutes = require('./routes/products');
-//const usersRoutes = require('./routes/users');
-//const ordersRoutes = require('./routes/orders');
+const usersRoutes = require('./routes/users');
+const ordersRoutes = require('./routes/orders');
 const api = process.env.API_URL;
 
-//app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/categories`, categoriesRoutes);
 app.use(`${api}/products`, productsRoutes);
-//app.use(`${api}/users`, usersRoutes);
-//app.use(`${api}/orders`, ordersRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
 //database
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/express_eshop', {
