2dc83b8fad753f19e42b841c6693d9af9f472614
sagara dissanayake <39671491+sagarromer@users.noreply.github.com>
2021-10-24 09:14:06 -0700

    login user endpoint generating a token

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,29 @@ router.post('/', async (req,res)=>{
 
     res.send(user);
 })
+router.post('/login', async (req,res) => {
+    const user = await User.findOne({email: req.body.email})
+    const secret = process.env.secret;
+    if(!user) {
+        return res.status(400).send('The user not found');
+    }
 
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+        const token = jwt.sign(
+            {
+                userId: user.id,
+                isAdmin: user.isAdmin
+            },
+            secret,
+            {expiresIn : '1d'}
+        )
+
+        res.status(200).send({user: user.email , token: token}) 
+    } else {
+        res.status(400).send('password is wrong!');
+    }
+
+
+})
 
 module.exports =router;
